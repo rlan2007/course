23a71c9507bcf7a936900afd6f2c5e0759f7ebc2
schooln5 <rlan2007@yandex.ru>
2022-02-03 16:15:49 +0500

    task 2.3.1 done

--- stepik/2/2.3.1/2.3.1.py
+++ stepik/2/2.3.1/2.3.1.py
@@ -1,21 +1,47 @@
 class multifilter:
     def judge_half(pos, neg):
         # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
+        return pos >= neg
 
     def judge_any(pos, neg):
         # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
+        return pos >= 1
 
     def judge_all(pos, neg):
         # допускает элемент, если его допускают все функции (neg == 0)
+        return neg == 0
 
     def __init__(self, iterable, *funcs, judge=judge_any):
         # iterable - исходная последовательность
-        self.interable = interable
+        self.iterable = iterable
         # funcs - допускающие функции
-        # judge - решающая функция
-
-    def __iter__(self):
-        # возвращает итератор по результирующей последовательности
+        self.funcs = funcs
+        self.judge = judge
+        self.pos = 0
+        self.neg = 0
+        self.index = -1   
+        
+    # возвращает итератор по результирующей последовательности
+    def __iter__(self):        
+      return self        
+        
+    def __next__(self):      
+      while(True):
+        self.pos, self.neg = 0, 0
+        self.index += 1
+        if self.index < len(self.iterable):
+          for fun in self.funcs:
+            if fun(self.iterable[self.index]):
+              self.pos+=1
+            else:
+              self.neg+=1        
+          # judge - решающая функция
+          if self.judge(self.pos,self.neg):          
+            return self.iterable[self.index]
+        else:
+          raise StopIteration
+          
+        
 
 # Пример использования:
 def mul2(x):
@@ -28,6 +54,7 @@ def mul5(x):
     return x % 5 == 0
 
 a = [i for i in range(31)]  # [0, 1, 2, ... , 30]
+
 
 print(list(multifilter(a, mul2, mul3, mul5)))
 # [0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30]
